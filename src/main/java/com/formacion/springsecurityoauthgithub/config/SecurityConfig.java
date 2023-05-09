98c49f753a16ffcbe38544f2fd33db8d43a3ab25
jorgeferalv@gmail.com <jorgeferalv@gmail.com>
2023-05-09 18:11:38 +0200

    Dev

---
+++ src/main/java/com/formacion/springsecurityoauthgithub/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.formacion.springsecurityoauthgithub.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.authorizeHttpRequests(authorize ->
+           authorize.anyRequest().authenticated()
+        ).oauth2Login(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+}
---
+++ src/test/java/com/formacion/springsecurityoauthgithub/SpringSecurityOauthGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formacion.springsecurityoauthgithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauthGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
